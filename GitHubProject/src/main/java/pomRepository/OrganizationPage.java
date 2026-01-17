22c038ec121f2ae9b386ac7e0dec7313f3fafea1
SAI <SAI@Vishwa>
2026-01-17 15:03:40 +0530

    push from Vishwa

---
+++ GitHubProject/src/main/java/pomRepository/OrganizationPage.java
@@ -0,0 +1,12 @@
+package pomRepository;
+
+public class OrganizationPage {
+	public static void main(String[] args) {
+		System.out.println("Created by vishwa");
+		
+		// Organization Page Created
+		
+		
+	}
+
+}
